d28eef52e35123cbd026b43da09dae054e12875d
Piotr <zamonagh@gmail.com>
2025-01-23 17:24:52 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroepswerkTaak1
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void OpenTab_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null) return;
+
+            string? tabName = button.Tag.ToString();
+            if (tabName == null) return;
+
+            var existingTab = tcMain.Items.OfType<TabItem>()
+                .FirstOrDefault(t => t.Header?.ToString() == tabName);
+
+            if (existingTab != null)
+            {                
+                tcMain.SelectedItem = existingTab;
+                return;
+            }
+
+            TabItem newTab = new TabItem
+            {
+                Header = tabName,
+                
+                
+            };
+
+            tcMain.Items.Add(newTab);
+            tcMain.SelectedItem = newTab;
+
+        }
+
+
+    }
+
+    
+
+
+
+
+}
