961a5191fa4642e7af3e568151d23bc213437233
DPabidax <abigit@abidax.com>
2025-02-09 18:38:51 +0100

    aamaak scherm Users

--- uc_Users.xaml.cs
+++ uc_Users.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
 
 namespace GroepswerkTaak1
 {
@@ -20,9 +22,103 @@ namespace GroepswerkTaak1
     /// </summary>
     public partial class uc_Users : UserControl
     {
+        #region  VARIABELEN
+        DataTable DTUsers;
+        DataTable DTRollen;
+        DataRow DR;
+        bool Loading = true;
+        bool IsUpdate = true; // false = Nieuws   , True = wijzigen
+        object ControlField;
+        #endregion
+
+        #region METHODES
         public uc_Users()
         {
             InitializeComponent();
         }
+
+
+        private void cboKeuze_Opvullen()
+        {
+            Loading = true;
+            try
+            {
+                this.Cursor = Cursors.Wait;
+                using (SqlConnection CN = new SqlConnection(Properties.Settings.Default.strCN))
+                {
+                    using (SqlCommand CMD = new SqlCommand(Properties., CN))
+                    {
+                        using (SqlDataAdapter DA = new SqlDataAdapter(CMD))
+                        {
+                            try
+                            {
+                                CMD.CommandType = CommandType.StoredProcedure;
+                                DT = new DataTable();
+                                DA.Fill(DT);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.ToString());
+                            }
+
+                        }
+                    }
+                }
+                cboKeuze.ItemsSource = DT.DefaultView;
+                cboKeuze.SelectedValuePath = "ProductID";
+                cboKeuze.DisplayMemberPath = "ProductName";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Er heeft zich een fout voorgedaan bij het opvullen van de lijst" + ex.ToString(), "opvul fout", MessageBoxButton.OK);
+            }
+            finally
+            {
+                this.Cursor = null;
+            }
+            Loading = false;
+        }
+
+
+        #endregion
+
+        #region EVENTS
+        private void cboKeuze_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cboMachteging_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnNieuw_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOpslaan_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVerwijderen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSluiten_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
     }
+
 }
