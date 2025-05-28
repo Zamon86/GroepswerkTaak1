1f307e39c38a777c124ca075406cffaef9e600b9
DPabidax <abigit@abidax.com>
2025-05-28 21:05:18 +0200

    invullen van de SP in de properties , aanmaak van de map viewmodels en bijhordende classe VM's

--- GroepswerkTaak1.DAL/clsKnoppenRepo.cs
+++ GroepswerkTaak1.DAL/clsKnoppenRepo.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using GroepswerkTaak1.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GroepswerkTaak1.DAL
 {
-    internal class clsKnoppenRepo
+    public class clsKnoppenRepo
     {
+        private ObservableCollection<clsKnoppenM> MijnCollectie;
+        int nr = 0;
+        public clsKnoppenRepo()
+        {
+        }
+
+        public bool Delete(clsKnoppenM entity)
+        { 
+        (DataTable DT, bool OK , string Boodschap)=
+                clsDAL.ExecuteDataTable(Properties.Resources.D_Knop,
+                clsDAL.Parameter("ID", entity.KnopId),
+                clsDAL.Parameter("User", Environment.UserName),
+                clsDAL.Parameter("ControlField", entity.ControlField),
+                clsDAL.Parameter("@ReturnValue", 0));
+            if (!OK)
+            { 
+            // entity.ErrorBoodschap = Boodschap;
+            }
+            return OK;
+        }
     }
 }
