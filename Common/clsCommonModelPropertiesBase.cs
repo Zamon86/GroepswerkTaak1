4c90564015fd9275a893439d8979c7f803a4fe77
DPabidax <abigit@abidax.com>
2025-05-29 15:23:01 +0200

    clsKnoppenRepo geschreven + clsKnoppenVM

---
+++ Common/clsCommonModelPropertiesBase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// ref cursus Peter Ceulemans 2025
+namespace Common
+{
+    public class clsCommonModelPropertiesBase : clsObservable
+    {
+        private bool _IsDirty = false;
+
+        public bool IsDirty
+        {
+            get { return _IsDirty; }
+            set { _IsDirty = value; OnPropertyChanged(); }
+        }
+
+        private int _MijnSelectedIndex;
+
+        public int MijnSelectedIndex
+        {
+            get { return _MijnSelectedIndex; }
+            set { _MijnSelectedIndex = value; OnPropertyChanged(); }
+        }
+
+        private int _MyVisibility;
+
+        public int MyVisibility
+        {
+            get { return _MyVisibility; }
+            set { _MyVisibility = value; OnPropertyChanged(); }
+        }
+
+        private int _MyVisibility_Contrary;
+
+        public int MyVisibility_Contrary
+        {
+            get { return _MyVisibility_Contrary; }
+            set { _MyVisibility_Contrary = value; OnPropertyChanged(); }
+        }
+
+        private object _ControlField;
+
+        public object ControlField
+        {
+            get { return _ControlField; }
+            set { _ControlField = value; OnPropertyChanged(); }
+        }
+
+        public string ErrorBoodschap { get; set; }
+
+        private List<string> _ErrorList;
+
+        public List<string> ErrorList
+        {
+            get { return _ErrorList; }
+            set { _ErrorList = value; }
+        }
+
+        public string Error
+        {
+            get
+            {
+                if (ErrorList.Count > 0)
+                {
+                    return "NOK";
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+    }
+}
--- GroepswerkTaak1.DAL/clsKnoppenRepo.cs
+++ GroepswerkTaak1.DAL/clsKnoppenRepo.cs
@@ -1,4 +1,5 @@
 ﻿using GroepswerkTaak1.Model;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -18,16 +19,83 @@ namespace GroepswerkTaak1.DAL
         }
 
         public bool Delete(clsKnoppenM entity)
-        { 
-        (DataTable DT, bool OK , string Boodschap)=
-                clsDAL.ExecuteDataTable(Properties.Resources.D_Knop,
-                clsDAL.Parameter("ID", entity.KnopId),
-                clsDAL.Parameter("User", Environment.UserName),
-                clsDAL.Parameter("ControlField", entity.ControlField),
-                clsDAL.Parameter("@ReturnValue", 0));
+        {
+            (DataTable DT, bool OK, string Boodschap) =
+                    clsDAL.ExecuteDataTable(Properties.Resources.D_Knop,
+                    clsDAL.Parameter("ID", entity.KnopId),
+                    clsDAL.Parameter("User", Environment.UserName),
+                    clsDAL.Parameter("ControlField", entity.ControlField),
+                    clsDAL.Parameter("@ReturnValue", 0));
             if (!OK)
-            { 
-            // entity.ErrorBoodschap = Boodschap;
+            {
+                entity.ErrorBoodschap = Boodschap;
+            }
+            return OK;
+        }
+
+        // public clsKnoppenM Find() { throw new NotImplementedException(); }  // not implemented
+
+        public ObservableCollection<clsKnoppenM> GetAll()
+        { GenerateCollection(); return MijnCollectie; }
+
+
+        private void GenerateCollection()
+        {
+            SqlDataReader MijnDataReader = clsDAL.GetData(Properties.Resources.S_Knop);
+            MijnCollectie = new ObservableCollection<clsKnoppenM>();
+            while (MijnDataReader.Read())
+            {
+                clsKnoppenM Knop = new clsKnoppenM()
+                {
+                    KnopId = (int)MijnDataReader["ID"],
+                    KnopNaam = MijnDataReader["Naam"].ToString(),
+                    KnopTekst = MijnDataReader["Tekst"].ToString(),
+                    KnopPositie = (int) MijnDataReader["Positie"],
+                    KnopImage = (byte[])MijnDataReader["Image"],
+                    ControlField = MijnDataReader["ControlField"]
+                };
+                MijnCollectie.Add(Knop);
+            }
+        }
+
+        public clsKnoppenM GetByID(int id)
+        {
+            if (MijnCollectie == null)
+            {
+                GenerateCollection();
+            }
+            return MijnCollectie.Where(x => x.KnopId == id).FirstOrDefault();
+        }
+
+        public bool Insert(clsKnoppenM entity)
+        {
+            (DataTable DT, bool OK, string Boodschap) =
+                    clsDAL.ExecuteDataTable(Properties.Resources.I_Knop,
+                    clsDAL.Parameter("Naam", entity.KnopNaam),
+                    clsDAL.Parameter("Tekst", entity.KnopTekst),
+                    clsDAL.Parameter("Positie", entity.KnopPositie),
+                    clsDAL.Parameter("Image", entity.KnopImage),
+                    clsDAL.Parameter("@ReturnValue", 0));
+            if (!OK)
+            {
+                entity.ErrorBoodschap = Boodschap;
+            }
+            return OK;
+        }
+        public bool Update(clsKnoppenM entity)
+        {
+            (DataTable DT, bool OK, string Boodschap) =
+                    clsDAL.ExecuteDataTable(Properties.Resources.I_Knop,
+                    clsDAL.Parameter("ID", entity.KnopId),
+                    clsDAL.Parameter("Naam", entity.KnopNaam),
+                    clsDAL.Parameter("Tekst", entity.KnopTekst),
+                    clsDAL.Parameter("Positie", entity.KnopPositie),
+                    clsDAL.Parameter("Image", entity.KnopImage),
+                    clsDAL.Parameter("ControlField", entity.ControlField),
+                    clsDAL.Parameter("@ReturnValue", 0));
+            if (!OK)
+            {
+                entity.ErrorBoodschap = Boodschap;
             }
             return OK;
         }
