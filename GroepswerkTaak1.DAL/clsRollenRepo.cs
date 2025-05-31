344a2d7aa7557131778f96e032edd9b3615a6bc3
DPabidax <abigit@abidax.com>
2025-05-31 12:13:01 +0200

    login procedure geschreven en uitgetest. loginscherm bevat de gegevens van de gebruiker

--- GroepswerkTaak1.DAL/clsRollenRepo.cs
+++ GroepswerkTaak1.DAL/clsRollenRepo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GroepswerkTaak1.DAL
 {
-    internal class clsRollenRepo
+    public class clsRollenRepo
     {
     }
 }
--- GroepswerkTaak1/Views/winLogon.xaml.cs
+++ GroepswerkTaak1/Views/winLogon.xaml.cs
@@ -1,13 +1,23 @@
-﻿using GroepswerkTaak1.Views;
+﻿using GroepswerkTaak1.DAL;
+using GroepswerkTaak1.Model;
+using GroepswerkTaak1.Views;
 using System.Windows;
 
 namespace GroepswerkTaak1
 {
+   
+
     /// <summary>
     /// Interaction logic for winLogon.xaml
     /// </summary>
     public partial class winLogon : Window
     {
+        int LoginId;
+        clsUsersM Gebruiker = new clsUsersM();
+        clsRollenM Rol = new clsRollenM();
+        clsLoginRepo LoginRepo = new clsLoginRepo();
+        clsUsersRepo UsersRepo = new clsUsersRepo();
+        clsRollenRepo RollenRepo = new clsRollenRepo();
         public winLogon()
         {
             InitializeComponent();
@@ -15,9 +25,29 @@ namespace GroepswerkTaak1
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow _MainWindow = new MainWindow();
-            _MainWindow.Show();
-            this.Close();
+            //contoleer Login
+            LoginId = LoginRepo.Login(txtLoginNaam.Text, txtLoginPwd.Password);
+            if (LoginId > 0)
+            {
+                MessageBox.Show("Login gelukt"); // nadien verwijderen, alleen voor test doeleinden
+                Gebruiker = UsersRepo.GetByID(LoginId);
+                MessageBox.Show(Gebruiker.Email);
+
+
+                //MainWindow _MainWindow = new MainWindow();
+                //_MainWindow.Show();
+                //this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Login mislukt, probeer het opnieuw.", "Foutmelding", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+
+            
 
         }
 
