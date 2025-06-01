ed018a2915781e716236543ffb62070f2c63a774
DPabidax <abigit@abidax.com>
2025-06-01 17:53:37 +0200

    integratie van "loggings" grid met filtermogelijkheden

--- GroepswerkTaak1/ViewModels/clsLoggingVM.cs
+++ GroepswerkTaak1/ViewModels/clsLoggingVM.cs
@@ -4,9 +4,11 @@ using GroepswerkTaak1.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace GroepswerkTaak1.ViewModels
 {
@@ -18,12 +20,95 @@ namespace GroepswerkTaak1.ViewModels
             get { return _Loggings; }
             set { _Loggings = value; }
         }
+
+        public ICollectionView LoggingsCV { get; }
+
+        private string _FilterAktie = string.Empty;
+
+        public string FilterAktie
+        {
+            get { return _FilterAktie; }
+            set { 
+                    _FilterAktie = value;
+                    OnPropertyChanged(nameof(FilterAktie));
+                    LoggingsCV.Refresh();
+            }
+        }
+
+        private string _FilterTabelColumn = string.Empty;
+        public string FilterTabelColumn
+        {
+            get { return _FilterTabelColumn; }
+            set
+            {
+                _FilterTabelColumn = value;
+                OnPropertyChanged(nameof(FilterTabelColumn));
+                LoggingsCV.Refresh();
+            }
+        }
+
+        private string _FilterPreValue = string.Empty;
+        public string FilterPreValue
+        {
+            get { return _FilterPreValue; }
+            set
+            {
+                _FilterPreValue = value;
+                OnPropertyChanged(nameof(FilterPreValue));
+                LoggingsCV.Refresh();
+            }
+        }
+        private string _FilterPostValue = string.Empty;
+        public string FilterPostValue
+        {
+            get { return _FilterPostValue; }
+            set
+            {
+                _FilterPostValue = value;
+                OnPropertyChanged(nameof(FilterPostValue));
+                LoggingsCV.Refresh();
+            }
+        }
+
+
+        private string _FilterGebruiker =  string.Empty;
+
+        public string FilterGebruiker
+        {
+            get { return _FilterGebruiker; }
+            set { 
+                    _FilterGebruiker = value; 
+                    OnPropertyChanged(nameof(FilterGebruiker)); 
+                    LoggingsCV.Refresh();
+            }
+        }
+
+
+
+
         public clsLoggingRepo repo = new clsLoggingRepo();
         public clsLoggingVM()
         {
             Loggings = repo.GetAll();
+            LoggingsCV = CollectionViewSource.GetDefaultView(Loggings);
+            LoggingsCV.Filter = new Predicate<object>(FilterLoggings);
         }
 
-
+        private bool FilterLoggings(object obj)
+        {
+            if (obj is clsLoggingM logging)
+            {
+                // Hier kun je de filtercriteria aanpassen
+                // Bijvoorbeeld: filteren op een specifieke gebruiker of datum
+                return
+                    logging.Aktie.Contains(_FilterAktie, StringComparison.InvariantCultureIgnoreCase) &&
+                    logging.TabelColumn.Contains(_FilterTabelColumn, StringComparison.InvariantCultureIgnoreCase) &&
+                    logging.PreValue.Contains(_FilterPreValue, StringComparison.InvariantCultureIgnoreCase) &&
+                    logging.PostValue.Contains(_FilterPostValue, StringComparison.InvariantCultureIgnoreCase) &&
+                    logging.Gebruiker.Contains(_FilterGebruiker, StringComparison.InvariantCultureIgnoreCase);
+            }
+       
+            return false; // Als het object geen clsLoggingM is, wordt het niet weergegeven
+        }
     }
 }
--- GroepswerkTaak1/Views/winLoggingFilter.xaml.cs
+++ GroepswerkTaak1/Views/winLoggingFilter.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using GroepswerkTaak1.DAL;
 using GroepswerkTaak1.Model;
+using GroepswerkTaak1.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -29,10 +30,29 @@ namespace GroepswerkTaak1.Views
         {
             InitializeComponent();
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+this.Close();
+        }
+
+        private void btnCleanFilters_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new clsLoggingVM (); // reset the DataContext to clear filters
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
     }
     /// Bronnen :
-    /// https://www.youtube.com/watch?v=wg1HUSIowTA&t=3s
+    /// https://www.youtube.com/watch?v=wg1HUSIowTA&t=3s  // opmaak grid + uitleg
     /// https://www.youtube.com/watch?v=nnxCO4JX1Wc
+    /// https://www.youtube.com/watch?v=fBKW-spQboc&t=2s  // filtering  + grouping
+    /// https://www.youtube.com/watch?v=wpBRHNDFXYI&t=8s  //extra uitleg
+    /// https://learn.microsoft.com/en-us/answers/questions/36999/how-to-speed-up-a-wpf-datagrid-bound-to-an-observa  //speeding up datagrid
     /// 
 
 
