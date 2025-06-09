2c23dadb39c2480ad40890f8c70e312a093eed0e
yannick joos <joos.yannick@live.be>
2025-06-09 15:49:45 +0200

    Basic mailclient

---
+++ GroepsWerk.Mail/UserLogin.cs
@@ -0,0 +1,10 @@
+﻿namespace GroepsWerk.Mail
+{
+    public interface UserLogin
+    {
+        string LoginNaam { get; set; }
+        string Wachtwoord { get; set; }
+
+        string Email { get; set; }
+    }
+}
