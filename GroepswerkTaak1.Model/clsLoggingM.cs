06156eaed060ca4e7a239b4e2c6c6b26a716fd16
PiotrZambrzycki <PiotrZambrzycki@C1429>
2025-06-17 18:39:46 +0200

    ?

--- GroepswerkTaak1.Model/clsLoggingM.cs
+++ GroepswerkTaak1.Model/clsLoggingM.cs
@@ -7,96 +7,96 @@ using System.Threading.Tasks;
 
 namespace GroepswerkTaak1.Model
 {
-    public class clsLoggingM : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler? PropertyChanged;
+		public class clsLoggingM : INotifyPropertyChanged
+		{
+				public event PropertyChangedEventHandler? PropertyChanged;
 
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+				private void OnPropertyChanged(string propertyName)
+				{
+						PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+				}
 
-        // Constructor
-        public clsLoggingM()
-        {
-        }
+				// Constructor
+				public clsLoggingM()
+				{
+				}
 
-        public clsLoggingM(int MyLogId, string MyAktie, string MyTabelColumn, string MyPreValue, string MyPostValue, string MyGebruiker)
-        {
-            _LogId = MyLogId;
-            _Aktie = MyAktie;
-            _TabelColumn = MyTabelColumn;
-            _PreValue = MyPreValue;
-            _PostValue = MyPostValue;
-            _Gebruiker = MyGebruiker;
+				public clsLoggingM(int MyLogId, string MyAktie, string MyTabelColumn, string MyPreValue, string MyPostValue, string MyGebruiker)
+				{
+						_LogId = MyLogId;
+						_Aktie = MyAktie;
+						_TabelColumn = MyTabelColumn;
+						_PreValue = MyPreValue;
+						_PostValue = MyPostValue;
+						_Gebruiker = MyGebruiker;
 
-        }
+				}
 
-        private int _LogId;
+				private int _LogId;
 
-        public int LogId
-        {
-            get { return _LogId; }
-            set { _LogId = value; }
-        }
+				public int LogId
+				{
+						get { return _LogId; }
+						set { _LogId = value; }
+				}
 
-        private string _Aktie;
+				private string _Aktie;
 
-        public string Aktie
-        {
-            get { return _Aktie; }
-            set { _Aktie = value; }
-        }
-
-
-        private string _TabelColumn;
-
-        public string TabelColumn
-        {
-            get { return _TabelColumn; }
-            set { _TabelColumn = value; }
-        }
-
-        private string _PreValue;
-
-        public string PreValue
-        {
-            get { return _PreValue; }
-            set { _PreValue = value; }
-        }
-
-        private string _PostValue;
-
-        public string PostValue
-        {
-            get { return _PostValue; }
-            set { _PostValue = value; }
-        }
-
-        private string _Gebruiker;
-
-        public string Gebruiker
-        {
-            get { return _Gebruiker; }
-            set { _Gebruiker = value; }
-        }
+				public string Aktie
+				{
+						get { return _Aktie; }
+						set { _Aktie = value; }
+				}
 
 
-        private DateTime _Registratie;
+				private string _TabelColumn;
 
-        public DateTime Registratie
-        {
-            get { return _Registratie; }
-            set { _Registratie = value; }
-        }
+				public string TabelColumn
+				{
+						get { return _TabelColumn; }
+						set { _TabelColumn = value; }
+				}
+
+				private string _PreValue;
+
+				public string PreValue
+				{
+						get { return _PreValue; }
+						set { _PreValue = value; }
+				}
+
+				private string _PostValue;
+
+				public string PostValue
+				{
+						get { return _PostValue; }
+						set { _PostValue = value; }
+				}
+
+				private string _Gebruiker;
+
+				public string Gebruiker
+				{
+						get { return _Gebruiker; }
+						set { _Gebruiker = value; }
+				}
 
 
-        private Object _ControlField;
-        public Object ControlField
-        {
-            get { return _ControlField; }
-            set { _ControlField = value; }
-        }
+				private DateTime _Registratie;
 
-    }
+				public DateTime Registratie
+				{
+						get { return _Registratie; }
+						set { _Registratie = value; }
+				}
+
+
+				private Object _ControlField;
+				public Object ControlField
+				{
+						get { return _ControlField; }
+						set { _ControlField = value; }
+				}
+
+		}
 }
